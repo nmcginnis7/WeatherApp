708cc0699aa8166cbb0dbbcc37dc19bd63ea8039
Nick McGinnis <nicholas.m.mcginnis@gmail.com>
2018-04-17 15:53:00 -0400

    final

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'b81bca6d709298d5f367f1017d83e1a5';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
